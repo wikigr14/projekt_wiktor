7f31fee0d167d4e4e7ed78053a921c645085c8a2
student <filipjjj2004@gmail.com>
2023-10-26 15:21:51 +0200

    02 zmiana mnozenia w metodzie policz

---
+++ cmake-build-debug/CMakeFiles/projekt_wiktor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt_wiktor.dir/main.cpp.obj"
+  "CMakeFiles/projekt_wiktor.dir/main.cpp.obj.d"
+  "libprojekt_wiktor.dll.a"
+  "projekt_wiktor.exe"
+  "projekt_wiktor.exe.manifest"
+  "projekt_wiktor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt_wiktor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projekt_wiktor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projekt_wiktor.
+# This may be replaced when dependencies are built.
